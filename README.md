327d64996967f19ac11e65bb9694eb0b4c2da62e
Rasul Gasbanov <rbagandaliev@yandex.ru>
2024-01-27 10:59:31 +0300

    перед ревью

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Проектная работа Mesto
 
+![index.html preview](./preview.png)
+
+Ссылки:
+
+Репозиторий проекта: https://github.com/LordMontroz/mesto-project-ff
+Проект опубликованный в gh-pages: https://lordmontroz.github.io/mesto-project-ff/
--- scripts/cards.js
+++ scripts/cards.js
@@ -1,32 +1,32 @@
 const initialCards = [
     {
       name: "Гоор",
-      link: "../images/goor.jpg",
+      link: "./images/goor.jpg",
       alt: "a man sitting on top of a lush green hillside"
     },
     {
       name: "Гамсутль",
-      link: "../images/gamsutl.jpg",
+      link: "./images/gamsutl.jpg",
       alt: "Gamsutl Village, now abandoned"
     },
     {
       name: "Сарыкум",
-      link: "../images/sarikum.jpg",
+      link: "./images/sarikum.jpg",
       alt: "dune Sarikum"
     },
     {
       name: "Сулакский каньон",
-      link: "../images/sulakcanyon.jpg",
+      link: "./images/sulakcanyon.jpg",
       alt: "Sulak canyon is one of the deepest canyons in the world and the deepest in Europe, its depth reaches up to 1920 meters and a length of 53 kilometers."
     },
     {
       name: "Гуниб",
-      link: "../images/mountain.jpg",
+      link: "./images/mountain.jpg",
       alt: "village Gunib"
     },
     {
       name: "Кайтаг",
-      link: "../images/kaitag.jpg",
+      link: "./images/kaitag.jpg",
       alt: "kaitag mountains"
     }
 ];
