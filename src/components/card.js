fa9802e2f9ce5dddf76963516722107d067af72a
Rasul Gasbanov <rbagandaliev@yandex.ru>
2024-03-16 16:03:37 +0300

    Добавлена работа с сервером, с помощью запроса получили карточки, лайки и т.д. Приступаю к работе с API

--- src/components/card.js
+++ src/components/card.js
@@ -2,6 +2,8 @@
 // функция создания карточки, функции-обработчики событий удаления
 // и лайка карточки;
 
+import { sendLike, deleteLike, removeCard } from "./index.js";
+
 /**
  * принимает в аргументах данные одной карточки
  * и функцию-колбэк для удаления,
@@ -20,6 +22,10 @@ function createCard(item, { deleteCard, openImage, likeCard }) {
     .content.querySelector(".card");
   const cardElementCopy = template.cloneNode(true);
   const likeButton = cardElementCopy.querySelector(".card__like-button");
+  const likeAmount = cardElementCopy.querySelector(".card__likes");
+  likeAmount.textContent = item.likes.length;
+
+  // const owner = card.owner_id === userId;
 
   const cardTitle = cardElementCopy.querySelector(".card__title");
   const cardImage = cardElementCopy.querySelector(".card__image");
@@ -31,7 +37,10 @@ function createCard(item, { deleteCard, openImage, likeCard }) {
   deleteBtn.addEventListener("click", deleteCard);
 
   cardImage.addEventListener("click", openImage);
-  likeButton.addEventListener("click", likeCard);
+
+  likeButton.addEventListener("click", (event) => {
+    likeCard(event, item._id);
+  });
 
   return cardElementCopy; //возвращаем карточку
 }
@@ -39,12 +48,26 @@ function createCard(item, { deleteCard, openImage, likeCard }) {
 // Функция удаления карточки
 function deleteCard(card) {
   const cardToRemove = card.target.closest(".card"); // target для элемента на котором произошло событие, closest - ищет родителя
+  removeCard(card, card._id);
   cardToRemove.remove(); // удаляем карточку
 }
 
 // функция лайка карточки
-function likeCard(evt) {
-  evt.target.classList.toggle("card__like-button_is-active");
+function likeCard(evt, id) {
+  const likeButton = evt.target;
+  const likesAmount = likeButton.nextElementSibling;
+
+  if (!likeButton.classList.contains("card__like-button_is-active")) {
+    sendLike(id).then((card) => {
+      likesAmount.textContent = card.likes.length;
+      likeButton.classList.toggle("card__like-button_is-active");
+    });
+  } else {
+    deleteLike(id).then((card) => {
+      likesAmount.textContent = card.likes.length;
+      likeButton.classList.toggle("card__like-button_is-active");
+    });
+  }
 }
 
 export { likeCard, deleteCard, createCard };
--- src/components/index.js
+++ src/components/index.js
@@ -44,6 +44,7 @@ const jobInput = formEditProfile.querySelector(
 );
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
+const profileImage = document.querySelector(".profile__image"); // фото профиля
 
 // форма добавления карточки
 const formElementPicture = document.querySelector(".popup_type_new-card");
@@ -63,6 +64,11 @@ function handleEditProfile(evt) {
   // вставляем новые значения
   profileTitle.textContent = nameInputValue;
   profileDescription.textContent = jobInputValue;
+  const profile = {
+    name: nameInput.value,
+    about: jobInput.value,
+  };
+  editProfile(profile); // отправляем данные на сервер
   closePopup(popupEdit);
 }
 
@@ -73,6 +79,7 @@ function handlePlaceFormSubmit(evt) {
     link: linkInput.value,
   };
   container.prepend(createCard(card, { deleteCard, openImage, likeCard }));
+  addNewCard(card); // отправляем данные на сервер
   closePopup(popupPlace);
 }
 
@@ -133,15 +140,111 @@ formElementPicture.addEventListener("submit", function (evt) {
 
 // объекты валидации
 const classes = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-}
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
 
 enableValidation(classes);
 
-// Вывести карточки на страницу
-initialCards.forEach(addCard);
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/wff-cohort-8",
+  headers: {
+    authorization: "5b217c83-6a50-4543-a844-91ed954409e0",
+    "Content-Type": "application/json",
+  },
+};
+
+// функция проверяет, что все хорошо
+function checkResponse(data) {
+  if (data.ok) {
+    return data.json();
+  }
+  return Promise.reject(`Ошибка: ${data.status}`);
+}
+
+// запрос GET: Вывод карточек на страницу
+const getInitialCards = () => {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  }).then(checkResponse);
+};
+
+// запрос GET: Загрузка информации о пользователе с сервера
+const getUserInfo = () => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    headers: config.headers,
+  }).then(checkResponse);
+};
+
+const setProfileData = (data) => {
+  profileTitle.textContent = data.name;
+  profileDescription.textContent = data.about;
+  profileImage.style = `background-image: url('${data.avatar}')`;
+};
+
+Promise.all([getInitialCards(), getUserInfo()])
+  .then(([cards, user]) => {
+    cards.forEach((card) => {
+      // console.log(card.likes.length);
+      addCard(card); // отрисовали картинки
+    });
+    setProfileData(user);
+  })
+  .catch((err) => {
+    console.log(`Ошибка: ${err}`);
+  });
+
+// запрос PATCH: Редактирование профиля
+const editProfile = (profile) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: profile.name,
+      about: profile.about,
+    }),
+  }).then(checkResponse);
+};
+
+// запрос POST: Добавление новой карточки
+const addNewCard = (card) => {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: card.name,
+      link: card.link,
+    }),
+  }).then(checkResponse);
+};
+
+// запрос DELETE: удаление карточки
+const removeCard = (card, id) => {
+  return fetch(`${config.baseUrl}/cards/${id}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then(checkResponse);
+};
+
+// запрос PUT: Добавляем лайк
+const sendLike = (id) => {
+  return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+    method: "PUT",
+    headers: config.headers,
+  }).then(checkResponse);
+  // .then((res) => console.log(res))
+};
+
+// запрос DELETE: Убираем лайк
+const deleteLike = (id) => {
+  return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then(checkResponse);
+};
+
+export { sendLike, deleteLike, removeCard };
