820ff0f3baabc75001643911decb7774665a50da
Rasul Gasbanov <rbagandaliev@yandex.ru>
2024-03-22 10:23:56 +0300

    realese/1.1.1

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,9 @@
 // Также в index.js находится код, который отвечает за отображение шести карточек при открытии страницы.
 
 import "/src/pages/index.css";
-import { initialCards } from "./cards.js";
+
 import { likeCard, deleteCard, createCard } from "./card.js";
+
 import {
   openPopup,
   closePopup,
@@ -44,6 +45,7 @@ const classes = {
 
 // DOM узлы
 const container = document.querySelector(".places__list"); // контейнер карточек
+
 // для кнопок и попапов
 const profileEditButton = document.querySelector(".profile__edit-button"); //кнопка редактирования
 const profileAddButton = document.querySelector(".profile__add-button"); // кнопка добавления карточки
